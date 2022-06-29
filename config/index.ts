ec64a0f236d95fb099cee25a5e34bd0ef33f945c
Marco <marco.fura@unmsm.edu.pe>
2022-06-28 21:36:50 -0500

    next-auth initial config done

---
+++ config/index.ts
@@ -0,0 +1,1 @@
+export { getSession, SessionProvider, signIn, signOut, useSession } from 'next-auth/react'
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,9 @@
 import { AppProps } from 'next/app'
-
+import { SessionProvider } from '../config'
 export default function MyApp ({ Component, pageProps: { session, ...pageProps } }:AppProps) {
   return (
+    <SessionProvider session={session} >
         <Component {...pageProps} />
+    </SessionProvider>
   )
 }
