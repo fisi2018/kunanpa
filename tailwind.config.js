bc9c814403f11128dac20b388f2e37cf216b56f3
Marco <marco.fura@unmsm.edu.pe>
2022-07-09 17:31:37 -0500

    them a-b-c-d-e moved to tailwnd config

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,13 @@ module.exports = {
           '0%': { transform: 'rotate(0deg)' },
           '100%': { transform: 'rotate(360deg)' }
         }
+      },
+      backgroundColor: {
+        'theme-a': '#C74544',
+        'theme-b': '#A32928',
+        'theme-c': '#E06665',
+        'theme-d': '#FC8887',
+        'theme-e': '#FFE0E0'
       }
 
     }
