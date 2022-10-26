fe3b7c9d960bcfc67987acdc911d5e185962cdf3
Marco <marco.fura@unmsm.edu.pe>
2022-10-26 00:02:11 -0500

    fix(loop-when-commit): fix loop generated when run git commit

--- views/profile/root/index.tsx
+++ views/profile/root/index.tsx
@@ -5,35 +5,37 @@ import CardProfile from './components/CardProfile'
 import { UserResponse } from './types/responses'
 
 type Props = {
-  categories: Category[],
-  userById:UserResponse
+    categories: Category[]
+    userById: UserResponse
 }
-/*const profile:IProfile = {
+/* const profile:IProfile = {
   _id: '2',
   address: 'Cercado de Lima',
   dni: '70481019',
   email: 'mjfura27@gmail.com',
   img: 'https://robohash.org/molestiasevenietnon.png?size=250x250&set=set4',
   name: 'Marco Fura'
-}*/
-export default function Profile ({ categories,userById }: Props) {
-  const routes: Route[] = [{
-    label: 'Inicio',
-    path: '/'
-  }, {
-    label: 'Perfil',
-    path: '/profile'
-  }]
-  
-  return (
-    <Layout categories={categories} routes={routes} >
-      <section className='min-h-screen p-4 flex justify-center items-center' >
+} */
+export default function Profile({ categories, userById }: Props) {
+    const routes: Route[] = [
         {
-          
-          <CardProfile profile={createProfileAdapter(userById)} />
-          
+            label: 'Inicio',
+            path: '/'
+        },
+        {
+            label: 'Perfil',
+            path: '/profile'
         }
-      </section>
-    </Layout>
-  )
+    ]
+
+    return (
+        <Layout
+            categories={categories}
+            routes={routes}
+        >
+            <section className="min-h-screen p-4 flex justify-center items-center">
+                {<CardProfile profile={createProfileAdapter(userById)} />}
+            </section>
+        </Layout>
+    )
 }
